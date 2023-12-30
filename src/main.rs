4c80faa8966269223f328274c0e449149ed0babd
Joseph T. Lyons <JosephTLyons@gmail.com>
2023-12-29 22:37:14 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,421 @@
+use std::fmt::Display;
+use std::time::Instant;
+
+use iced::widget::{button, column, container, radio, text, text_input, Column, Row, Space, Text};
+use iced::{executor, Application, Command, Element, Length, Renderer, Settings, Theme};
+use tap_tempo::TapTempo;
+
+#[derive(Debug, Clone)]
+enum Unit {
+    Milliseconds,
+    Hertz,
+}
+
+impl Unit {
+    fn toggle(&self) -> Self {
+        match self {
+            Unit::Milliseconds => Unit::Hertz,
+            Unit::Hertz => Unit::Milliseconds,
+        }
+    }
+}
+
+impl Display for Unit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Unit::Milliseconds => write!(f, "ms"),
+            Unit::Hertz => write!(f, "Hz"),
+        }
+    }
+}
+
+enum RhythmicModifier {
+    Normal,
+    Dotted,
+    Triplet,
+}
+
+const RHYTHMIC_MODIFIER: [RhythmicModifier; 3] = [
+    RhythmicModifier::Normal,
+    RhythmicModifier::Dotted,
+    RhythmicModifier::Triplet,
+];
+
+impl RhythmicModifier {
+    fn value(&self) -> f64 {
+        match self {
+            RhythmicModifier::Normal => 1.0,
+            RhythmicModifier::Dotted => 1.5,
+            RhythmicModifier::Triplet => 2.0 / 3.0,
+        }
+    }
+}
+
+impl Display for RhythmicModifier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RhythmicModifier::Normal => write!(f, "Normal"),
+            RhythmicModifier::Dotted => write!(f, "Dotted"),
+            RhythmicModifier::Triplet => write!(f, "Triplet"),
+        }
+    }
+}
+
+enum NoteValue {
+    Whole,
+    Half,
+    Quarter,
+    Eighth,
+    Sixteenth,
+    ThirtySecond,
+    SixtyFourth,
+    HundredTwentyEighth,
+}
+
+impl NoteValue {
+    fn value(&self) -> f64 {
+        match self {
+            NoteValue::Whole => 4.0,
+            NoteValue::Half => 2.0,
+            NoteValue::Quarter => 1.0,
+            NoteValue::Eighth => 0.5,
+            NoteValue::Sixteenth => 0.25,
+            NoteValue::ThirtySecond => 1.0 / 8.0,
+            NoteValue::SixtyFourth => 1.0 / 16.0,
+            NoteValue::HundredTwentyEighth => 1.0 / 32.0,
+        }
+    }
+}
+
+impl Display for NoteValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let note = match self {
+            NoteValue::Whole => "1",
+            NoteValue::Half => "1/2",
+            NoteValue::Quarter => "1/4",
+            NoteValue::Eighth => "1/8",
+            NoteValue::Sixteenth => "1/16",
+            NoteValue::ThirtySecond => "1/32",
+            NoteValue::SixtyFourth => "1/64",
+            NoteValue::HundredTwentyEighth => "1/128",
+        };
+
+        write!(f, "{}", note)
+    }
+}
+
+const NOTE_VALUES: [NoteValue; 8] = [
+    NoteValue::Whole,
+    NoteValue::Half,
+    NoteValue::Quarter,
+    NoteValue::Eighth,
+    NoteValue::Sixteenth,
+    NoteValue::ThirtySecond,
+    NoteValue::SixtyFourth,
+    NoteValue::HundredTwentyEighth,
+];
+
+const SPACING: u16 = 15;
+
+pub fn main() -> iced::Result {
+    Tap::run(Settings {
+        window: iced::window::Settings {
+            size: (650, 600),
+            min_size: Some((650, 600)),
+            max_size: None,
+            ..iced::window::Settings::default()
+        },
+        ..Settings::default()
+    })
+}
+
+struct Tap {
+    tap_tempo: TapTempo,
+    tempo: Option<f64>,
+    last_tap: Option<Instant>,
+    tempo_input_text: String,
+    unit: Unit,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Tap,
+    ManualReset,
+    ScaleTempo(f64),
+    TempoInputSubmitted,
+    TempoInputChanged(String),
+    UpdateUnit,
+}
+
+impl Default for Tap {
+    fn default() -> Self {
+        let default_tempo = 120.0;
+
+        Self {
+            tap_tempo: TapTempo::new(),
+            tempo: Some(default_tempo),
+            last_tap: None,
+            tempo_input_text: default_tempo.to_string(),
+            unit: Unit::Milliseconds,
+        }
+    }
+}
+
+impl Application for Tap {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_: Self::Flags) -> (Self, Command<Message>) {
+        (Self::default(), Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Tap Tempo")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::Tap => {
+                self.tempo = self.tap_tempo.tap();
+                self.last_tap = Some(Instant::now());
+                match self.tempo {
+                    Some(tempo) => self.tempo_input_text = format!("{:.3}", tempo),
+                    None => self.tempo_input_text = "N/A".to_string(),
+                }
+            }
+            Message::ManualReset => {
+                self.tap_tempo.reset();
+                self.last_tap = None;
+            }
+            Message::ScaleTempo(scale) => {
+                if let Some(tempo) = self.tempo {
+                    self.tempo = Some(tempo * scale);
+                    self.tempo_input_text = format!("{:.3}", self.tempo.unwrap());
+                }
+            }
+            Message::TempoInputSubmitted => self.tempo = self.tempo_input_text.parse().ok(),
+            Message::TempoInputChanged(text) => self.tempo_input_text = text,
+            Message::UpdateUnit => self.unit = self.unit.toggle(),
+        }
+
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        let tap_count = self.tap_tempo.tap_count();
+        let tap_button_text = Text::new(if tap_count == 0 {
+            "Tap".to_string()
+        } else {
+            format!("Tap ({})", tap_count)
+        });
+
+        let (ms_selected, hz_selected) = match self.unit {
+            Unit::Milliseconds => (Some(()), None),
+            Unit::Hertz => (None, Some(())),
+        };
+
+        let controls_row = Row::with_children(vec![
+            button(tap_button_text)
+                .on_press(Message::Tap)
+                .width(75)
+                .into(),
+            button("Reset")
+                .on_press(Message::ManualReset)
+                .width(75)
+                .into(),
+            text_input("", self.tempo_input_text.as_str())
+                .on_input(|text| Message::TempoInputChanged(text))
+                .on_submit(Message::TempoInputSubmitted)
+                .into(),
+            button("Halve")
+                .on_press(Message::ScaleTempo(0.5))
+                .width(75)
+                .into(),
+            button("Double")
+                .on_press(Message::ScaleTempo(2.0))
+                .width(75)
+                .into(),
+            radio(Unit::Milliseconds.to_string(), (), ms_selected, |_| {
+                Message::UpdateUnit
+            })
+            .into(),
+            radio(Unit::Hertz.to_string(), (), hz_selected, |_| {
+                Message::UpdateUnit
+            })
+            .into(),
+        ])
+        .spacing(SPACING);
+
+        let table = table(self.tempo, &self.unit);
+        let column = column![
+            controls_row,
+            Space::with_height(SPACING),
+            table.height(Length::Fill)
+        ];
+
+        container(column).padding(SPACING).into()
+    }
+
+    // fn subscription(&self) -> Subscription<Message> {
+    //     if self.last_tap.is_some() {
+    //         return Subscription::new();
+    //     }
+
+    //     Subscription::none()
+    // }
+}
+
+fn table<'a>(tempo: Option<f64>, unit: &Unit) -> Row<'a, Message, Renderer> {
+    let mut note_labels: Vec<Element<_>> = vec![
+        text("").height(Length::Fill).into(), // Is there a better way to add a blank cell?
+    ];
+
+    note_labels.extend(NOTE_VALUES.map(|note_value| {
+        text(format!("{}:", note_value.to_string()))
+            .height(Length::Fill)
+            .into()
+    }));
+
+    let note_label_column = Column::with_children(note_labels.into()).height(Length::Fill);
+
+    let mut table: Vec<Element<_>> = vec![note_label_column.width(Length::Fill).into()];
+
+    for rhythmic_modifier in &RHYTHMIC_MODIFIER {
+        table.push(
+            values_column(tempo, rhythmic_modifier, unit)
+                .width(Length::Fill)
+                .into(),
+        );
+    }
+
+    Row::with_children(table)
+}
+
+fn values_column<'a>(
+    tempo: Option<f64>,
+    rhythmic_modifier: &RhythmicModifier,
+    unit: &Unit,
+) -> Column<'a, Message, Renderer> {
+    let mut column: Vec<Element<_>> = vec![text(rhythmic_modifier.to_string())
+        .height(Length::Fill)
+        .into()];
+    column.extend(NOTE_VALUES.map(|note_value| {
+        Text::new(match tempo {
+            Some(tempo) => {
+                let value = match unit {
+                    Unit::Milliseconds => calculate_ms(tempo, &note_value, rhythmic_modifier),
+                    Unit::Hertz => calculate_hz(tempo, &note_value, rhythmic_modifier),
+                };
+                format!("{:.3} {}", value, unit.to_string())
+            }
+            None => "N/A".to_string(),
+        })
+        .height(Length::Fill)
+        .into()
+    }));
+
+    Column::with_children(column)
+}
+
+fn calculate_ms(
+    tempo: f64,
+    note_modifier: &NoteValue,
+    rhythmic_modifier: &RhythmicModifier,
+) -> f64 {
+    (1.0 / (tempo / 60_000.0)) * note_modifier.value() * rhythmic_modifier.value()
+}
+
+fn calculate_hz(
+    tempo: f64,
+    note_modifier: &NoteValue,
+    rhythmic_modifier: &RhythmicModifier,
+) -> f64 {
+    60.0 / (tempo * note_modifier.value() * rhythmic_modifier.value())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_ms() {
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::Whole, &RhythmicModifier::Normal),
+            2000.0
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::Half, &RhythmicModifier::Normal),
+            2000.0 / 2.0
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::Quarter, &RhythmicModifier::Normal),
+            2000.0 / 4.0
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::Eighth, &RhythmicModifier::Normal),
+            2000.0 / 8.0
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::Sixteenth, &RhythmicModifier::Normal),
+            2000.0 / 16.0
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::ThirtySecond, &RhythmicModifier::Normal),
+            2000.0 / 32.0
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::SixtyFourth, &RhythmicModifier::Normal),
+            2000.0 / 64.0
+        );
+
+        assert_eq!(
+            calculate_ms(
+                120.0,
+                &NoteValue::HundredTwentyEighth,
+                &RhythmicModifier::Normal
+            ),
+            2000.0 / 128.0
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::Quarter, &RhythmicModifier::Dotted),
+            2000.0 / 4.0 * 1.5
+        );
+
+        assert_eq!(
+            calculate_ms(120.0, &NoteValue::Quarter, &RhythmicModifier::Triplet),
+            2000.0 / (4.0 * (3.0 / 2.0))
+        );
+    }
+
+    #[test]
+    fn test_calculate_hz() {
+        let value = calculate_hz(120.0, &NoteValue::Quarter, &RhythmicModifier::Normal);
+        assert_eq!(value, 2.0);
+
+        let value = calculate_hz(120.0, &NoteValue::Eighth, &RhythmicModifier::Normal);
+        assert_eq!(value, 4.0);
+
+        let value = calculate_hz(180.0, &NoteValue::Quarter, &RhythmicModifier::Dotted);
+        assert_eq!(value, 2.0);
+
+        let value = calculate_hz(200.0, &NoteValue::Quarter, &RhythmicModifier::Triplet);
+        assert_eq!(value, 5.0);
+    }
+}
+
+// TODO - fix hertz via tests
+// TODO - auto reset tap tempo
+// TODO - reverse input
+// TODO - keyboard driven
+// TODO - copy to click
+// TODO - styling
+// TODO - scaling UI
